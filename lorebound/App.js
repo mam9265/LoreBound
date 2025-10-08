91ff49d5f9afe3762c83fe260c9f4f35bfff454c
mam9265 <mam9265@psu.edu>
2025-10-08 10:16:45 -0400

    Remove Old Instances of Frontend

--- lorebound/App.js
+++ lorebound/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
