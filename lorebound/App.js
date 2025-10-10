5ad9eb411955ce7438d985fc2b0a14c827bcd332
mam9265 <mam9265@psu.edu>
2025-10-10 11:04:07 -0400

    Updated Frontend

--- lorebound/App.js
+++ lorebound/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground, Image } from 'react-native';
 
 export default function App() {
   const [name, setName] = React.useState('');
 
-  const handlePress = () => {
-    alert(`Hello, ${name || 'stranger'}!`);
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to My App!</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your name"
-        value={name}
-        onChangeText={setName}
-      />
-      <Button title="Say Hello" onPress={handlePress} />
+      {/* Title */}
+      <Text style={styles.title}>
+        <Text style={styles.lore}>Lore</Text>
+        <Text style={styles.bound}>Bound</Text>
+      </Text>
+      {/* Buttons */}
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Dungeon Select</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Daily Challenge</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.smallButton}>
+        <Text style={styles.buttonText}>Leaderboards</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -27,18 +32,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#a5d7e8',
   },
   title: {
-    fontSize: 24,
-    marginBottom: 16,
+    fontSize: 48,
+    fontWeight: '600',
+    marginBottom: 40,
     textAlign: 'center',
+    color: '#19376d'
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 12,
-    borderRadius: 5,
+  lore:{
+    color: '#ffffff',
+  },
+  bound:{
+    color: '#19376d',
+  },
+  button: {
+    backgroundColor: '#19376d',
+    paddingVertical: 16,
+    paddingHorizontal: 50,
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+  smallButton: {
+    backgroundColor: '#5260e0',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    marginBottom: 40,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 22,
+    fontFamily: 'serif',
+    textTransform: 'capitalize',
   },
 });
